38772735fc12bd85c5f463ed7943898f13768457
AnoudAlshnakat <ano.o@live.com>
2026-05-10 02:06:55 +0200

    check if this is the problem

--- hol/polygram/policy_test_cases/prepp.sh
+++ hol/polygram/policy_test_cases/prepp.sh
@@ -21,14 +21,14 @@ cd ../policy_test_cases
 Holmake "paper_exampleTheory.uo"
 
 
-for i in {1..7}; do
-    timeout 600s Holmake "internet_firewall_${i}Theory.uo"
-    EXIT_CODE=$?
-    if [ $EXIT_CODE -eq 124 ]; then
-        echo "TIMEOUT: internet_firewall_${i}Theory.uo exceeded 600s seconds, skipping."
-    elif [ $EXIT_CODE -ne 0 ]; then
-        echo "FAILED: internet_firewall_${i}Theory.uo exited with code $EXIT_CODE."
-    else
-        echo "OK: internet_firewall_${i}Theory.uo completed successfully, time log file is in .hol/logs"
-    fi
-done
+# for i in {1..7}; do
+#     timeout 600s Holmake "internet_firewall_${i}Theory.uo"
+#     EXIT_CODE=$?
+#     if [ $EXIT_CODE -eq 124 ]; then
+#         echo "TIMEOUT: internet_firewall_${i}Theory.uo exceeded 600s seconds, skipping."
+#     elif [ $EXIT_CODE -ne 0 ]; then
+#         echo "FAILED: internet_firewall_${i}Theory.uo exited with code $EXIT_CODE."
+#     else
+#         echo "OK: internet_firewall_${i}Theory.uo completed successfully, time log file is in .hol/logs"
+#     fi
+# done
